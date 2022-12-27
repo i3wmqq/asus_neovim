0018a3b14ad5a4b4da7920082461807af94c4e5c
Kay Nguyen <i3wmqq@gmail.com>
2022-12-26 23:18:28 -0600

    init

--- lua/kay/treesitter.lua
+++ lua/kay/treesitter.lua
@@ -1,15 +1,15 @@
 local treesitter_status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
 if not treesitter_status_ok then
-	return
+  return
 end
 
 treesitter.setup({
-	indent = {
-		enable = true,
-	},
-	ensure_installed = "all",
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = true,
-	},
+  indent = {
+    enable = true,
+  },
+  ensure_installed = { "c", "cpp", "markdown", "bash", "yaml", "html" },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = true,
+  },
 })
